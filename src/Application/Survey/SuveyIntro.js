316b38ef936840435ba5a852b9b3f876a69f6ad8
Nathan Rogan <nathan.rogan@wmca.org.uk>
2022-06-16 13:50:32 +0100

    spelling updates

--- src/Application/Survey/SuveyIntro.js
+++ src/Application/Survey/SuveyIntro.js
@@ -9,9 +9,23 @@ const SurveyIntro = () => {
         sectionName="LACF Survey"
       />
       <h2>Cycling for Everyone Cycle Hire activity survey.</h2>
-      <p>Thank you for choosing to participate in Cycling for Everyone - Cycle Hire. Before you participate in the cycle hire scheme, Transport for West Midlands would like to ask you some questions about your current travel and the journeys that you typically make.</p>
-      <p>This information will be extremely useful in helping us to understand the impact of Cycling for Everyone - Cycle Hire on those who have taken part.</p>
-      <p>There are 25 questions. It should take you no more than 5 minutes to fill in. All completed entries will be eligible for entry into a prize draw with the chance to <b>win one prize of £50 in Amazon Vouchers</b>. The closing date for entries is the last day of the month of you applied. </p>
+      <p>
+        Thank you for choosing to participate in Cycling for Everyone - Cycle
+        Hire. Before you participate in the cycle hire scheme, Transport for
+        West Midlands would like to ask you some questions about your current
+        travel and the journeys that you typically make.
+      </p>
+      <p>
+        This information will be extremely useful in helping us to understand
+        the impact of Cycling for Everyone - Cycle Hire on those who have taken
+        part.
+      </p>
+      <p>
+        It should take you no more than 5 minutes to fill in. All completed
+        entries will be eligible for entry into a prize draw with the chance to{" "}
+        <b>win one prize of £50 in Amazon Vouchers</b>. The closing date for
+        entries is the last day of the month you applied.
+      </p>
     </FormSection>
   );
 };
