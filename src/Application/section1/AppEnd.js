7eb3b723b206e95638767aef3615045bf8171250
Nathan Rogan <rolorogan@gmail.com>
2023-05-30 11:02:03 +0100

    add promotion end message

---
+++ src/Application/section1/AppEnd.js
@@ -0,0 +1,17 @@
+import FormSection from "../../common/FormSection";
+import Header from "../../common/Header";
+
+const AppEnd = () => {
+  return (
+    <>
+      <Header heading="Apply for support with West Midlands Cycle Hire costs" />
+      <FormSection>
+        <div className="wmnds-container wmnds-p-t-lg">
+        <p>This promotion has now ended. </p>
+        </div>
+      </FormSection>
+    </>
+  );
+};
+
+export default AppEnd;
